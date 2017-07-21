e26a45b565ec71bcd68a65e541c17074ab44ab4d
Shreyans Dhaddha <shreyansdha_ddha@hotmail.com>
2017-07-20 20:49:41 -0700

    First commit

---
+++ VDynamics/Controllers/HelpController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VDynamics.Controllers
+{
+    public class HelpController : Controller
+    {
+        public ActionResult Index()
+        {
+			List<string> Countries = new List<string>()
+			{
+				"India", "China", "US", "UK"
+			};
+
+			ViewBag.Countries = Countries;
+
+            return View ();
+        }
+    }
+}
+
+
